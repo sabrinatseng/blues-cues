38c5360975ebd475c3571caa4d5787aedb2bc27f
Sabrina Tseng <stseng@mit.edu>
2020-09-19 09:26:59 -0700

    Scaffolding

---
+++ main.py
@@ -0,0 +1,14 @@
+def main():
+    """
+    Runs Blue's Cues alongside a Zoom call.
+    """
+    print("Hello world")
+    # probably a loop here
+    # get audio input
+    # get video input
+    # process audio input
+    # process video input
+    # update display
+
+if __name__ == "__main__":
+    main()
